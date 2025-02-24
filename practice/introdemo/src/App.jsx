d5aad9d6f3d808d263d508f97b07b67f6f6bf1cb
Your Name <you@example.com>
2025-02-24 16:50:08 +0300

    "part1 exercise start - unicafe"

--- practice/introdemo/src/App.jsx
+++ practice/introdemo/src/App.jsx
@@ -1,26 +1,79 @@
-// const App = () => {
-//   const friends = [
-//     { name: 'Peter', age: 4 },
-//     { name: 'Maya', age: 10 },
-//   ]
+import { useState } from "react";
+
+// const History = (props) => {
+//   if (props.allClicks.length === 0) {
+//     return (
+//       <div>
+//         the app is used by pressing the buttons
+//       </div>
+//     )
+//   }
 
 //   return (
 //     <div>
-//       <p>{friends[0].name} {friends[0].age}</p>
-//       <p>{friends[1].name} {friends[1].age}</p>
+//       button press history: {props.allClicks.join(' ')}
 //     </div>
 //   )
 // }
 
-// export default App
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
 
+// const Button = ( props ) => {
+//   console.log(props)
+//   const {onClick,text} = props
+//   return(  <button onClick={onClick}>
+//     {text}
+//   </button>
+//   )
+// }
+
+// const App = () => {
+//   const [left, setLeft] = useState(0)
+//   const [right, setRight] = useState(0)
+//   const [allClicks, setAll] = useState([])
+
+//   const handleLeftClick = () => {
+//     setAll(allClicks.concat('L'))
+//     setLeft(left + 1)
+//   }
+
+//   const handleRightClick = () => {
+//     setAll(allClicks.concat('R'))
+//     setRight(right + 1)
+//   }
+
+//   return (
+//     <div>
+//       {left}
+
+//       <Button onClick={handleLeftClick} text='left' />
+//       <Button onClick={handleRightClick} text='right' />
+//       {right}
+//       <History allClicks={allClicks} />
+//     </div>
+//   )
+// }
+
+const Button = (props) => (
+  <button onClick={props.onClick}> {props.text} </button>
+)
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
   return (
     <div>
-      <p>{friends}</p>
+      {value}
+
+      <Button onClick={() => setToValue(1000)} text = "thousand"/>
+      <Button onClick={() => setToValue(0)} text = "reset"/>
+      <Button onClick={() => setToValue(value + 1)} text = "increment"/>
     </div>
   )
 }
 
-export default App
+export default App;
