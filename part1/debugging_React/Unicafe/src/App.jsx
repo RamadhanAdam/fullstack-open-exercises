94e7265e8e02b1cc6785b2527ca89e1ed554ece7
Your Name <you@example.com>
2025-02-24 18:15:36 +0300

    "step 2 initial stage"

--- part1/debugging_React/Unicafe/src/App.jsx
+++ part1/debugging_React/Unicafe/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const handleGood = () =>{
+    const updateGood = good + 1
+    setGood(updateGood)
+  }
+  const handleNeutral = () =>{
+    const updateNeutral =  neutral + 1
+    setNeutral(updateNeutral)
+  }
+  const handleBad = () =>{
+    const updateBad =  bad + 1
+    setBad(updateBad)
+  }
+
+  const total = (good + bad + neutral)
+  const avg = total === 0 ? 0:  (good * 1 + bad * 0 + neutral *-1)/total
+  const percentage = good/total
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <Button  onClick = {handleGood} text = "good"/>
+      <Button onClick = {handleNeutral} text = "neutral" />
+      <Button  onClick = {handleBad} text = "bad" />
+
+      <h1>statistics</h1>
+      <p>good : {good}</p>
+      <p>neutral : {neutral}</p>
+      <p>bad : {bad}</p>
+      <p>all : {total}</p>
+      <p>average : {avg}</p>
+      <p>percentage : {percentage}</p>
+    </div >
+
   )
 }
 
-export default App
+export default App;
