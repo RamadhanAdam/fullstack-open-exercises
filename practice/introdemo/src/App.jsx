364ccdf649b43be35bbfca9d3177cdb50cb79919
Your Name <you@example.com>
2025-02-28 13:01:40 +0300

    "part2"

--- practice/introdemo/src/App.jsx
+++ practice/introdemo/src/App.jsx
@@ -1,14 +1,36 @@
-// import { useState } from "react";
-import Note from './components/Note'
-const App = ({ notes }) => {
+import { useState } from "react";
+
+const App = () => {
+  const [formData, setFormData] = useState({
+    name: " ",
+    message: " "
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return { ...prevFormData, [name]: value };
+    });
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name ${formData.name}, message: ${formData.message}`)
+  }
+
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map((note) => <Note key = {note.id} note = {note}/>)}
-      </ul>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name : </label>
+      <input name="name" type="text" value={formData.name} onChange={handleChange} />
+
+      <br /><br />
+      <label htmlFor="message">Message : </label>
+      <input name="message" type="text" value={formData.value} onChange={handleChange} />
+
+      <br /><br />
+      <button type="submit" >Submit</button>
+    </form>
   )
 }
 
-export default App
+export default App;
