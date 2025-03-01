33830d1095bf67003ab1f1e1aba347714d4c3d67
RamadhanAdam <ramadhanzome@gmail.com>
2025-03-01 15:09:39 +0300

    Part2 section a

--- part1/Intro_to_React/application/src/App.jsx
+++ part1/Intro_to_React/application/src/App.jsx
@@ -1,41 +1,39 @@
-import {Courses} from './Course.jsx'
+import { courses } from "./Course"  
 
-const Total = ({totalprops}) => {
-  const total = totalprops.reduce((sum,currentVal) => sum +=currentVal.exercises ,0)
-  return <p><b>Total number of exercises is {total} </b></p>
-}
+const Header = (props) => <h2>{props.course}</h2>
 
-const Header = ( props ) => {
-  return <h1>{props.name}</h1>
-}
-
-const Part = (props) => {
+const Content = ({ parts }) => {
   return (
-      <p>{props.parts.name} {props.parts.exercises}</p>
-  );
-};
+  parts.map(part => <Part key={part.id} part={part} />)
+    )
+  }
 
-const Content = (props) => {
-  return (   
-    props.partsprops1.map((element) => <Part key ={element.id} parts= {element} />)
-  );
-};
+const Part = ({part}) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
 
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) =>  sum += part.exercises, 0)
+  return (
+    <h3>total of exercises {total}</h3>
+  )
+}
 
 const App = () => {
-  const courses = Courses()
-
+console.log(courses)
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      <Header name={courses[0].name} />
-      <Content partsprops1={courses[0].parts} />
-      <Total totalprops={courses[0].parts} />
-      <Header name={courses[1].name} />
-      <Content partsprops1={courses[1].parts} />
-      <Total totalprops={courses[1].parts} />
+      <h1>Web development Curriculum</h1>
+      <Header course={courses[0].name} />
+      <Content parts={courses[0].parts} />
+      <Total parts= {courses[0].parts}/>
+      <Header course={courses[1].name} />
+      <Content parts={courses[1].parts} />
+      <Total parts= {courses[1].parts}/>
     </div>
   )
 }
 
-export default App;
+export default App
---
+++ part2/notes/src/components/Notes.jsx
@@ -0,0 +1,6 @@
+const Note = (props) => {
+    const note = props.note
+   return ( <li>{note.content}</li>)
+  }
+
+  export default Note
